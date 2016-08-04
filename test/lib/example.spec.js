ef585cf552a12ce566567e08dc997d53c438ad1d
tec7z7 <jared.martin@vitals.com>
2016-08-04 17:59:43 -0500

    Initial commit.

--- test/lib/example.spec.js
+++ test/lib/example.spec.js
@@ -1,10 +1,23 @@
 const { expect } = require('chai')
 const Example = require('../../lib/example')
 
-describe('Example', () => {
-  it('tests something', () => {
-    expect(Example.render('{foo}', {
-      foo: 'bar',
-    })).to.eq('bar')
+describe('checking pyramid', () => {
+  it('when column is 1 and row is 1', () => {
+    expect(Example.getNum(0, 0)).to.eq(1)
+  })
+  it('when column is 0 and row is 1', () => {
+    expect(Example.getNum(0, 1)).to.eq(1)
+  })
+  it('when column is 2 and row is 3', () => {
+    expect(Example.getNum(1, 2)).to.eq(2)
+  })
+  it('when column is 3 and row is 6', () => {
+    expect(Example.getNum(2, 5)).to.eq(10)
+  })
+  it('when row is 5', () => {
+    var start = new Date().getTime();
+    Example.drawTree(25)
+    var stop = new Date().getTime();
+    console.log(stop-start);
   })
 })
